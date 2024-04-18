62f934b8611729bc5cf25f0f2d95241d50a0699e
Xyasth <Pkosasih@student.ciputra.ac.id>
2024-04-18 12:10:57 +0700

    Initial Commit

---
+++ SwiftUITutorials/SwiftUITutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialsApp.swift
+//  SwiftUITutorials
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
