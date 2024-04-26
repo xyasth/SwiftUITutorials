62b0e8f593be8e54b3542ec95c8f22537474747e
Xyasth <Pkosasih@student.ciputra.ac.id>
2024-04-26 10:15:38 +0700

    Chapter 3

---
+++ SwiftUITutorials/Views/Category/Categoryitem.swift
@@ -0,0 +1,34 @@
+//
+//  Category item.swift
+//  SwiftUITutorials
+//
+//  Created by student on 25/04/24.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .foregroundStyle(.primary)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+}
+
+#Preview {
+    Category_item()
+}
